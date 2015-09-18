4a77d794dee6821c2671460de08aa75507f86d6b
Eoin Ó Loideáin <eoloideain@gmail.com>
2015-09-18 23:12:28 +0100

    data for scatter stored in controller

--- app/scatter/scatter.directive.js
+++ app/scatter/scatter.directive.js
@@ -9,8 +9,10 @@
 
   function scatter($window, $timeout ,d3Service) {
     return {
-      restrict: 'EA',
-      scope: {},
+      //restrict: 'EA',
+      //scope: {},
+      restrict: 'E',
+      scope: { data: '=' },
       link: function (scope, element, attrs) {
         d3Service.d3().then(function (d3) {
 
@@ -18,7 +20,7 @@
             width = 600 - margin.left - margin.right,
             height = 700 - margin.top - margin.bottom;
 
-          var parseDate = d3.time.format('%d-%b-%y').parse;
+          var parseDate = d3.time.format("%d-%b-%y").parse;
 
           var x = d3.time.scale()
             .range([0, width]);
@@ -48,47 +50,44 @@
             .append('g')
             .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-          // Hard coded data
-          scope.data = [
-            {date: '4-Apr-12', close: 34},
-            {date: '5-Apr-12', close: 45},
-            {date: '6-Apr-12', close: 37},
-            {date: '7-Apr-12', close: 56},
-            {date: '8-Apr-12', close: 50},
-            {date: '9-Apr-12', close: 77}
-          ];
 
-          scope.data.forEach(function (d) {
-            d.date = parseDate(d.date);
-            d.close = +d.close;
-          });
+          scope.$watch('data', function(data){
 
-          x.domain(d3.extent(scope.data, function (d) {
-            return d.date;
-          }));
-          y.domain(d3.extent(scope.data, function (d) {
-            return d.close;
-          }));
+            scope.data.forEach(function (d) {
+              console.log(d);
+              d.date = parseDate(d.date);
+              d.close = +d.close;
+            });
 
-          svg.append('g')
-            .attr('class', 'x axis')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(xAxis);
+            //console.log(scope.data);
+            x.domain(d3.extent(scope.data, function (d) {
+              return d.date;
+            }));
+            y.domain(d3.extent(scope.data, function (d) {
+              return d.close;
+            }));
 
-          svg.append('g')
-            .attr('class', 'y axis')
-            .call(yAxis)
-            .append('text')
-            .attr('transform', 'rotate(-90)')
-            .attr('y', 6)
-            .attr('dy', '.71em')
-            .style('text-anchor', 'end')
-            .text('Price ($)');
+            svg.append('g')
+              .attr('class', 'x axis')
+              .attr('transform', 'translate(0,' + height + ')')
+              .call(xAxis);
 
-          svg.append('path')
-            .datum(scope.data)
-            .attr('class', 'line')
-            .attr('d', line);
+            svg.append('g')
+              .attr('class', 'y axis')
+              .call(yAxis)
+              .append('text')
+              .attr('transform', 'rotate(-90)')
+              .attr('y', 6)
+              .attr('dy', '.71em')
+              .style('text-anchor', 'end')
+              .text('Price ($)');
+
+            svg.append('path')
+              .datum(scope.data)
+              .attr('class', 'line')
+              .attr('d', line);
+
+          }, true);
 
         });
       }
--- app/scatter/scatter.js
+++ app/scatter/scatter.js
@@ -5,10 +5,33 @@
     .module('app.scatter')
     .controller('scatter', scatter);
 
-  scatter.$inject = ['$scope', '$window'];
+  scatter.$inject = ['$scope', 'd3Service'];
 
-  function scatter($scope, $window){
-    $scope.awesomeThings = [
-    ];
-  }
+
+    function scatter($scope, d3Service) {
+      d3Service.d3().then(function (d3) {
+        /*var parseDate = d3.time.format("%d-%b-%y").parse;
+        d3.json('app/content/data1.json', function (err, data) {
+          if (err) {
+            throw err;
+          }
+          $scope.data1 = data;
+
+          $scope.data.forEach(function (d) {
+            d.date = parseDate(d.date);
+            d.close = +d.close;
+          });
+
+          $scope.apply();
+        });*/
+        $scope.data2 = [
+          {"date": "4-Apr-12", "close": 34},
+          {"date": "5-Apr-12", "close": 45},
+          {"date": "6-Apr-12", "close": 37},
+          {"date": "7-Apr-12", "close": 56},
+          {"date": "8-Apr-12", "close": 50},
+          {"date": "9-Apr-12", "close": 77}
+        ];
+      });
+    }
 })();
