f1aed5206f8627e3772195ada4ffaf47f705f5c9
Eoin Ó Loideáin <eoloideain@gmail.com>
2015-09-21 22:38:22 +0100

    controller for taking in proverbs data added

---
+++ app/pattern/pattern.js
@@ -0,0 +1,23 @@
+(function (){
+  'use strict';
+
+  angular
+    .module('app.pattern')
+    .controller('pattern', pattern);
+
+  pattern.$inject = ['$scope', 'd3Service'];
+
+  function pattern($scope, d3Service) {
+    d3Service.d3().then(function (d3) {
+      d3.json('app/content/proverbs.json', function (err, data) {
+        if (err) {
+          throw err;
+        }
+
+        $scope.data = data;
+        $scope.$apply();
+
+      });
+    });
+  }
+})();
